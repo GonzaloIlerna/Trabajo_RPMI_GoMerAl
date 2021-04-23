b24e9955a1e7846c45d66d2532ebb69495021c0d
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-04-23 09:06:39 +0200

    Base creada
    Hemos creado la base del proyecto en .html .css y .js entre los tres integrantes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("btn")
+
+function empezar(){
+    document.getElementById("inicio").style.display = "none"
+    document.getElementById("principal").style.display = "block"
+}
+
+btn.addEventListener("click",empezar)
