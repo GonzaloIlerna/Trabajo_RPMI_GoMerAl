d81362a1334434b2e2927b205cbc65c9b5cd7a83
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-04-28 13:14:57 +0200

    Actualizado css
    He puesto muis dos id y los he modificado un poco

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-let btn = document.getElementById("btn")
+let btn = document.getElementById("btn");
 
 function empezar(){
     document.getElementById("inicio").style.display = "none"
     document.getElementById("principal").style.display = "block"
-}
+};
 
-btn.addEventListener("click",empezar)
+btn.addEventListener("click",empezar);
+
+$("#G_2").draggable();
+
+$("#G_2").click(function(){
+    $G_2
+
+});
