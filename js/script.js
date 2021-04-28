81baade1f6f2e236541c9449f06bd366a85b4e21
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-04-28 14:47:48 +0200

    He cambiado mis 2 figuras
    H ecmaviado las funciones que hacen y los fondos

--- js/script.js
+++ js/script.js
@@ -7,9 +7,28 @@ function empezar(){
 
 btn.addEventListener("click",empezar);
 
+
+
+
+
+// $("#G_2").click(function(){
+//     $G_2
+
+// });
+
+$("#G_1").click(function(){
+    $("#principal").css("background", "repeating-radial-gradient(black, black 5px, white 5px, white 10px)")
+})
+
+let texto = document.getElementById("expl");
+
+$("#G_1").click(function(){
+    $("#principal").css("background", "repeating-radial-gradient(black, black 5px, white 5px, white 10px)")
+    $("#expl").css("color", "red")
+    texto.textContent ='Ya lo has encontrado y para volver a cambiarlo pulsa el cuadrado de abajo a la derecha';
+    console.log(texto.textContent);
+})
+
 $("#G_2").draggable();
 
-$("#G_2").click(function(){
-    $G_2
-
-});
+$("#G_1").draggable();
