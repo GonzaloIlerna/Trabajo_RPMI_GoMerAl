c9072f025a32982b5df9f275a632d3e2da0105bb
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-04-29 13:18:03 +0200

    He añadido cosas al css y al js
    He añadido dos figuras mas y un párrafo mas, he modificado el css también

--- js/script.js
+++ js/script.js
@@ -20,6 +20,14 @@ $("#G_1").click(function(){
     $("#principal").css("background", "repeating-radial-gradient(black, black 5px, white 5px, white 10px)")
 })
 
+$("#G_3").click(function(){
+    $("#principal").css("background", "linear-gradient(30deg, rgb(255, 190, 241), white)")
+})
+
+$("#G_4").click(function(){
+    $("#principal").css("background", "linear-gradient(30deg, rgb(255, 190, 241), white)")
+})
+
 let texto = document.getElementById("expl");
 
 $("#G_1").click(function(){
@@ -32,3 +40,12 @@ $("#G_1").click(function(){
 $("#G_2").draggable();
 
 $("#G_1").draggable();
+
+let ocultar = document.getElementById("G_4");
+
+function quitar(){
+    document.getElementById("expl").style.display = "none"
+    document.getElementById("p2").style.display = "none"
+};
+
+ocultar.addEventListener("dblclick",quitar);
