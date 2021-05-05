d60e68f762e38b95c8838612db09f1b1a9fc12e5
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-05-05 14:42:53 +0200

    Cambiado cosas del símbolo illuminati hay que terminar de arreglarlo
    He añadido también un cuadrado para poder dibujar, tengo que quitar elementos que tengo demasiados

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ function empezar(){
 btn.addEventListener("click",empezar);
 
 
+$("#G_6c").hide();
 
+$("#G_7c").hide();
 
 
 // $("#G_2").click(function(){
@@ -48,10 +50,21 @@ function quitar(){
 ocultar.addEventListener("dblclick",quitar);
 
 $("#G_5").droppable({
+    accept: "#G_6",
+    
     drop: function(event, ui){
-        $("#G_6c").show();
-    }
-});
+    $("#G_6c").show();
+    $("#G_6").hide();
+}});
+
+$("#G_5").droppable({
+    accept: "#G_7",
+    
+    drop: function(event, ui){
+    $("#G_7c").show();
+    $("#G_7").hide();
+}});
+
 
 $("#G_6").draggable();
 
@@ -66,3 +79,101 @@ $("#G_1").draggable();
 // Hacer un dragabble revert y tambien un dropable que cuando sea afirmativo el 
 // el dropable se desactive el dragable y se quede donde esta el objeto/imagen
 // hacer que se desactive todo y no se pueda mover mas ese objeto
+
+
+    // VARIABLES
+    //======================================================================
+    let miCanvas = document.querySelector('#pizarra');
+    let lineas = [];
+    let correccionX = 0;
+    let correccionY = 0;
+    let pintarLinea = false;
+    // Marca el nuevo punto
+    let nuevaPosicionX = 0;
+    let nuevaPosicionY = 0;
+
+    let posicion = miCanvas.getBoundingClientRect()
+    correccionX = posicion.x;
+    correccionY = posicion.y;
+
+    miCanvas.width = 500;
+    miCanvas.height = 500;
+
+    //======================================================================
+    // FUNCIONES
+    //======================================================================
+
+    /**
+     * Funcion que empieza a dibujar la linea
+     */
+    function empezarDibujo () {
+        pintarLinea = true;
+        lineas.push([]);
+    };
+    
+    /**
+     * Funcion que guarda la posicion de la nueva línea
+     */
+    function guardarLinea() {
+        lineas[lineas.length - 1].push({
+            x: nuevaPosicionX,
+            y: nuevaPosicionY
+        });
+    }
+
+    /**
+     * Funcion dibuja la linea
+     */
+    function dibujarLinea (event) {
+        event.preventDefault();
+        if (pintarLinea) {
+            let ctx = miCanvas.getContext('2d')
+            // Estilos de linea
+            ctx.lineJoin = ctx.lineCap = 'round';
+            ctx.lineWidth = 1;
+            // Color de la linea
+            ctx.strokeStyle = '#fff';
+            // Marca el nuevo punto
+            if (event.changedTouches == undefined) {
+                // Versión ratón
+                nuevaPosicionX = event.layerX;
+                nuevaPosicionY = event.layerY;
+            } else {
+                // Versión touch, pantalla tactil
+                nuevaPosicionX = event.changedTouches[0].pageX - correccionX;
+                nuevaPosicionY = event.changedTouches[0].pageY - correccionY;
+            }
+            // Guarda la linea
+            guardarLinea();
+            // Redibuja todas las lineas guardadas
+            ctx.beginPath();
+            lineas.forEach(function (segmento) {
+                ctx.moveTo(segmento[0].x, segmento[0].y);
+                segmento.forEach(function (punto, index) {
+                    ctx.lineTo(punto.x, punto.y);
+                });
+            });
+            ctx.stroke();
+        }
+    }
+
+    /**
+     * Funcion que deja de dibujar la linea
+     */
+    function pararDibujar () {
+        pintarLinea = false;
+        guardarLinea();
+    }
+
+    //======================================================================
+    // EVENTOS
+    //======================================================================
+
+    // Eventos raton
+    miCanvas.addEventListener('mousedown', empezarDibujo, false);
+    miCanvas.addEventListener('mousemove', dibujarLinea, false);
+    miCanvas.addEventListener('mouseup', pararDibujar, false);
+
+    // Eventos pantallas táctiles
+    miCanvas.addEventListener('touchstart', empezarDibujo, false);
+    miCanvas.addEventListener('touchmove', dibujarLinea, false);
