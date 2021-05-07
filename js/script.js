d3e33bf2cec21cc82447ea0bb7358510a16be81c
Gonzalo Romero <gromero@alumnos.ilernasevilla.com>
2021-05-07 09:23:00 +0200

    Todo terminado
    Me falta ponerlo todo organizado

--- js/script.js
+++ js/script.js
@@ -7,16 +7,9 @@ function empezar(){
 
 btn.addEventListener("click",empezar);
 
-
 $("#G_6c").hide();
 
 $("#G_7c").hide();
-
-
-// $("#G_2").click(function(){
-//     $G_2
-
-// });
 
 $("#G_1").click(function(){
     $("#principal").css("background", "repeating-radial-gradient(black, black 5px, white 5px, white 10px)")
@@ -26,21 +19,9 @@ $("#G_3").click(function(){
     $("#principal").css("background", "linear-gradient(30deg, rgb(255, 190, 241), white)")
 })
 
-$("#G_4").click(function(){
-    $("#principal").css("background", "linear-gradient(30deg, rgb(255, 190, 241), white)")
-})
-
 let texto = document.getElementById("expl");
 
-$("#G_1").click(function(){
-    $("#principal").css("background", "repeating-radial-gradient(black, black 5px, white 5px, white 10px)")
-    $("#expl").css("color", "red")
-    texto.textContent ='Ya lo has encontrado y para volver a cambiarlo pulsa el cuadrado de abajo a la derecha';
-    console.log(texto.textContent);
-})
-
-
-let ocultar = document.getElementById("G_4");
+let ocultar = document.getElementById("G_5");
 
 function quitar(){
     document.getElementById("expl").style.display = "none"
@@ -52,19 +33,41 @@ ocultar.addEventListener("dblclick",quitar);
     $("#G_5").droppable({
              
         drop: function(event, ui){
+            if(G_7bool==true){
             $("#G_7c").show();
             $("#G_7").hide();
         }
+        if (G_6bool==true){
+            $("#G_6c").show();
+            $("#G_6").hide();
+        }
+    }
         
-    });
+});
 
 
+let G_7bool=false;
+
+let G_6bool=false;
 
 
+$( "#G_6" ).draggable({
+    start: function( event, ui ) {
+            G_6bool=true;
+    },
+    stop: function( event, ui ) {
+      G_6bool=false;
+  }
+  });
 
-$("#G_6").draggable();
-
-$("#G_7").draggable();
+$( "#G_7" ).draggable({
+    start: function( event, ui ) {
+            G_7bool=true;
+    },
+    stop: function( event, ui ) {
+      G_7bool=false;
+  }
+  });
 
 $("#G_2").draggable();
 
